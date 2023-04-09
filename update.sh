6ba40478ff8cfe97cd65b74aab0461c021df6535
Elliotte Rusty Harold <elharo@ibiblio.org>
2023-04-09 10:54:28 -0400

    wip

--- update.sh
+++ update.sh
@@ -7,13 +7,15 @@ git branch 39
 mvn clean
 
 # update parent pom
-find . -name 'pom.xml' -print0 | xargs -0 sed -i '' 's/<version>3\d<\/version>/<version>39<\/version>/g'
+find . -name 'pom.xml' -print0 | xargs -0 sed -i '' 's/<version>3[0-8]<\/version>/<version>39<\/version>/g'
 
 # strip existing licenses
 find . -name '*.java' -print | ~/update-maven-project/strip_license.py
 
 # reformat
 mvn spotless:apply
+
+it
 
 git commit "update parent POM to 39"
 git push
